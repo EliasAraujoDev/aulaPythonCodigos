e1b7ea144e12acae8180edbd57a33a8567e17f51
EliasAraujoDev <eliasaraujo1222@outlook.com>
2024-03-28 22:04:07 -0300

    Aula Alura

---
+++ jogo/jogoAdivinhacao.py
@@ -0,0 +1,26 @@
+print("****************")
+print("Bem vindo ao jogo de Adivinhação")
+print("****************")
+
+numero_secreto = 49
+chute = input("Digite o numero Secreto: ")
+
+
+print("Você degitou", chute, sep=' ')
+
+chute = int(chute)
+
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+
+if (acertou):
+    print("Você acertou")
+else:
+    if(maior):
+        print("Seu Chute é maior que o numero secreto")
+    elif(menor):
+        print("Seu Chute foi menor que o numero secreto")
+    
+print("Fim de jogo!")
